e8564c17c2eb9aeba73c083fe6323644f3d9b3ab
Astrr44 <81384383+Astrr44@users.noreply.github.com>
2021-05-23 14:42:08 +0100

    Update README.py

--- README.py
+++ README.py
@@ -5,14 +5,38 @@ import sys
 import random
 
 
-def win():
-    print('O Jogo: Tu ganhaste, queres uma medalha?')
+import time
+import sys
+import random
 
-def lost():
-    print('O Jogo: Tu perdeste...ganda BOT')
 
-def draw():
-    print('O Jogo: Empate.')
+print("\t\|_______                           _______|/")
+print("\t\|___           Bem vindo(a)!           ___|/")
+print("\t\|__________                     __________|/")
+print("\t\|________ Ao pedra, papel, tesoura _______|/")  
+print("\t (͡ ͡° ͜ つ ͡͡°)                  (͡ ͡° ͜ つ ͡͡°)    ")
+time.sleep(1)
+
+print("...loading [■■■■■■■□] 60%")
+time.sleep(1)
+print("...loading [■■■■■■■■□] 67%")
+time.sleep(1)
+print("...loading [■■■■■■■■■□] 81%")
+time.sleep(1)
+print("...loading [■■■■■■■■■] 97%")
+time.sleep(1.6)
+print("→_→...loading [■■■■■■■■■] 99%")
+time.sleep(0.6)
+
+
+def ganhou():
+    print("Máquina: Tu ganhaste, queres uma medalha?")
+
+def perdeu():
+    print("Máquina: Tu perdeste...ganda BOT")
+
+def Empate():
+    print ("Máquina: Empate")
 
 
 def O_Jogo():
@@ -23,44 +47,43 @@ def O_Jogo():
     tentativas = 3
     while True:
         if tentativas == 0:
-            print('O Jogo: Acabaram se as tuas tentativas. Xauuuu.')
-            time.sleep(4)
+            print('Máquina: Acabaram se as tuas tentativas.... Xau.')
+            time.sleep(3)
             sys.exit(0)
-
-        player = str(input('Player: ')).lower()
-        if player.lower() not in moves:
-            print(f'O Jogo: Opçôes inválidas! tu tens ainda {tentativas} tentativas.')
+        Jogador = str(input('Jogador: ')).lower()
+        if Jogador.lower() not in moves:
+            print(f'Máquina: Opçôes inválidas! tu tens ainda {tentativas} tentativas.')
             tentativas -= 1
         else:
             máquina = random.choice(moves)
             print(f'máquina: {máquina}')
 
-            if player == máquina:
-                draw()
-            elif player == 'Pedra' and máquina == 'tesoura':
-                win()
-            elif player == 'pedra' and máquina == 'papel':
-                lost()
-            elif player == 'papel' and máquina  == 'pedra':
-                win()
-            elif player == 'papel' and máquina == 'tesoura':
-                lost()
-            elif player == 'tesoura' and máquina == 'papel':
-                win()
-            elif player == 'tesoura' and máquina == 'pedra':
-                lost()
+            if Jogador == máquina:
+                Empate()
+            elif Jogador == 'Pedra' and máquina == 'tesoura':
+                ganhou()
+            elif Jogador == 'pedra' and máquina == 'papel':
+                perdeu()
+            elif Jogador == 'papel' and máquina  == 'pedra':
+                ganhou()
+            elif Jogador == 'papel' and máquina == 'tesoura':
+                perdeu()
+            elif Jogador == 'tesoura' and máquina == 'papel':
+                ganhou()
+            elif Jogador == 'tesoura' and máquina == 'pedra':
+                perdeu()
             else:
                 pass
 
-            play_again = str(input('O Jogo: Queres tentar outraves? (s/n): '))
+            play_again = str(input('Máquina: Queres Jogar outra? (s/n): '))
             if play_again.lower() == 's':
                 O_Jogo()
             elif play_again.lower() == 'n':
-                print('O Jogo: Game Over. Xau.')
+                print('Máquina: Game Over. Xau.')
                 time.sleep(2.5)
                 sys.exit(0)
             else:
-                print('O Jogo: Opções inválidas. Xau...')
+                print('Máquina: Opções inválidas. Xau...')
                 time.sleep(2.5)
                 sys.exit(0)
 
